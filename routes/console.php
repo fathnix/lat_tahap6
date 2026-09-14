51c9559914db33ec17c0c350f3c18b5166b6b7fa
fathnix <mhmd.fatih2106@gmail.com>
2026-09-14 08:06:16 +0700

    Penerapan schedule job namun belum di coba

--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,17 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+
+Schedule::call(function(){
+    $jumlah = DB::table('products')->whereDate('created_at', today())->count();
+
+    Log::info("Hari ini ada {$jumlah} barang yang di titipkan! ");
+})->dailyAt('23:00');
